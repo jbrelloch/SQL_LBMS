737ff2cc6d414cf7b7f7172f6ca2cde878ec758a
jbrelloch <jbrelloch@gmail.com>
2013-11-14 15:13:38 -0500

    Initial content

---
+++ AssociationMiner/AssociationMiner/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssociationMiner
+{
+    class Program
+    {
+        private static int requiredFrequency = 500;
+
+        private static Dictionary<int[], Double> aprioriResults = new Dictionary<int[], Double>();
+        private static List<int[]> dataSetLines;
+
+        static void Main(string[] args)
+        {
+            if(args.Length != 1)
+            {
+                Console.WriteLine("AssociationMiner [Frequency]");
+                return;
+            }
+            List<string> fileLines = new List<string>();	
+            //READ LINES - IN MY CASE READ IN DATABASE ENTRIES
+		
+		    int maxValue = getMaxValue(fileLines);
+		    dataSetLines = convertEntriesToIntArrays(fileLines);
+		
+		    bool continueRun = true;
+		    int count = 1;
+		    while(continueRun)
+		    {
+			    getFrequencyNumbers(count);
+			
+			    trimMap(count);
+			
+			    continueRun = getStatus(count);
+			    count++;
+		    }
+		
+		    //int count = 0;
+		    //for(int numFreq : numberFrequencies)
+		    //{
+		    //	System.out.println(count + " FREQ: " + numFreq);
+		    //	count++;
+		    //}
+        }
+
+        public static int getMaxValue(List<String> fileLines)
+	    {
+		    int maxValue = 0;
+            //for(String line : fileLines)
+            //{
+            //    String[] splitFileLine = line.split(" ");
+            //    for(String lineSec : splitFileLine)
+            //    {
+            //        String trimLineSec = lineSec.trim();
+            //        int tempValue = -1;
+            //        try
+            //        {
+            //            tempValue = Integer.parseInt(trimLineSec);
+            //        }
+            //        catch (NumberFormatException e)
+            //        {
+            //            e.printStackTrace();
+            //        }
+            //        if(tempValue > maxValue)
+            //            maxValue = tempValue;
+            //    }
+            //}
+		    return maxValue;
+	    }
+
+        public static void trimMap(int count)
+	    {
+            //LinkedList<int[]> removeList = new LinkedList<int[]>();
+            ////first convert all base counts to percentages
+            //for(Map.Entry<int[], Double> entry : aprioriResults.entrySet())
+            //{
+            //    if(entry.getKey().length == count)
+            //    {
+            //        double holder = entry.getValue() / 100000;
+            //        entry.setValue(holder);
+            //        if(holder < requiredFrequency)
+            //        {
+            //            removeList.add(entry.getKey());
+            //        }
+            //    }
+            //}
+		
+            ////next cut out all mapEntries that do not meet the minimum base frequency
+            //for(int[] removeItemKey : removeList)
+            //{
+            //    aprioriResults.remove(removeItemKey);
+            //}
+	    }
+
+        public static void getFrequencyNumbers(int count)
+	    {
+            foreach(int[] line in dataSetLines)
+            {
+                List<int[]> combinations = getCombinations(line, count);
+                foreach(int[] combo in combinations)
+                {
+                    if(aprioriResults.ContainsKey(combo))
+                    {
+                        double holder = aprioriResults[combo] + 1;
+                        aprioriResults[combo] = holder;
+                    }
+                    else
+                    {
+                        aprioriResults[combo] = 1.0;
+                    }
+                }
+            }
+	    }
+
+        public static List<int[]> getCombinations(int[] line, int count)
+	    {
+            //GETS ALL COMBINATIONS FOR CURRENT ENTRY
+
+		    List<int[]> pSet = new List<int[]>();
+		
+		    int elements = line.Length;
+		
+		    int powerElements = (int) Math.Pow(2, elements);
+		
+		    for (int i = 0; i < powerElements; i++)
+		    {
+			    String binaryCount = Convert.ToString(i,2);
+			
+			    List<int> tempSet = new List<int>();
+			
+			    for (int j = 0; j < elements; j++)
+			    {
+				    if(binaryCount[j]=='1')
+				    {
+					    tempSet.Add(line[j]);
+				    }
+			    }
+			
+			    pSet.Add(tempSet.ToArray());
+		    }
+		
+		    //trim the full powerset
+		    foreach(int[] sublist in pSet)
+		    {
+			    if(sublist.Length == count)
+			    {
+				    int[] tempArray = new int[count];
+				
+				
+			    }
+		    }
+
+            return pSet;
+	    }
+
+        public static bool getStatus(int count)
+	    {
+            // THIS FUNCTION CHECKS TO SEE IF ANY FREQUENT SETS WHERE FOUND IN LAST RUN
+
+            //Set<int[]> mapKeySet = aprioriResults.keySet();
+		
+            //for(int[] key : mapKeySet)
+            //{
+			
+            //}
+		
+		    return false;
+	    }
+
+        public static List<int[]> convertEntriesToIntArrays(List<string> fileLines)
+	    {
+            //SEPERATE EACH ENTRY INTO AN ARRAY OF ITEM IDS
+		    List<int[]> results = new List<int[]>();
+		    foreach(string line in fileLines)
+		    {
+			    string[] splitFileLine = line.Split(' ');
+			    int[] arrayHolder = new int[splitFileLine.Length];
+                for (int i = 0; i < splitFileLine.Length; i++)
+			    {
+				    String trimLineSec = splitFileLine[i].Trim();
+				    int value = -1;
+				    
+                    value = int.Parse(trimLineSec);
+
+				    arrayHolder[i] = value;
+			    }
+			    results.Add(arrayHolder);
+		    }
+		    return results;
+	    }
+    }
+}
---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+<head>
+
+	<title>Jason Brelloch CSC 8711</title>
+	<link rel="stylesheet" href="http://bootswatch.com/slate/bootstrap.css" media="screen">
+    <link rel="stylesheet" href="http://bootswatch.com/assets/css/bootswatch.min.css">
+
+    <script type="text/javascript" src="./Bootswatch_Slate_files/ga.js"></script>
+	<script type="text/javascript" src="./Bootswatch_Slate_files/bsa.js"></script>
+	<script type="text/javascript" id="_bsap_js_c466df00a3cd5ee8568b5c4983b6bb19" src="./Bootswatch_Slate_files/s_c466df00a3cd5ee8568b5c4983b6bb19.js"></script>
+
+</head>
+
+<body style="">
+	
+	<div class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+	        <div class="navbar-header">
+	          	<a href="index.php" class="navbar-brand">Home</a>
+	        </div>
+	        <div class="navbar-collapse collapse" id="navbar-main">
+	        	<ul class="nav navbar-nav">
+		            <li class="dropdown">
+		            	<a class="dropdown-toggle" data-toggle="dropdown" id="browse">Browse Books <span class="caret"></span></a>
+		            	<ul class="dropdown-menu" aria-labelledby="browse">
+			                <li><a tabindex="-1" href="#">Textbooks</a></li>
+			                <li><a tabindex="-1" href="#">Historical</a></li>
+			                <li><a tabindex="-1" href="#">Biographies</a></li>
+			                <li class="divider"></li>
+			                <li><a tabindex="-1" href="#">Fantasy</a></li>
+			                <li><a tabindex="-1" href="#">Science Fiction</a></li>
+			                <li><a tabindex="-1" href="#">Romance</a></li>
+		            	</ul>
+		            </li>
+				</ul>
+
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">Book Cart</a></li>
+					<li><a href="#">Profile/Login</a></li>
+				</ul>
+
+	        </div>
+      	</div>
+	</div>
+
+	<div class="container">
+
+		<div class="page-header" id="banner">
+			<div class="row">
+				<div class="col-lg-6">
+					<h1>Library Manangement System</h1>
+					<p class="lead">Browse and Checkout Books</p>
+					<br/><br/>
+					<br/><br/>
+					<blockquote class="pull-right">
+		            	<p>The best Library Management System ever!</p>
+		            	<small>Some Guy <cite title="Source Title">Somewhere</cite></small>
+		            </blockquote>
+				</div>
+				<div class="col-lg-6">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-1">
+							</div>
+							<div class="col-lg-11">
+								<img src="./Bootswatch_Slate_files/books.png" id="bsap_1277971" class="bsap_1277971 bsap"></img>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="bs-docs-section">
+
+	        <div class="row">
+	          <div class="col-lg-12">
+	            <div class="bs-example">
+	              <div class="jumbotron">
+	                <h1>Jumbotron</h1>
+	                <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+	                <p><a class="btn btn-primary btn-lg">Learn more</a></p>
+	              </div>
+	            </div>
+	          </div>
+	        </div>
+	    </div>
+    </div>
+
+	<script src="./Bootswatch_Slate_files/jquery.min.js"></script>
+    <script src="./Bootswatch_Slate_files/bootstrap.min.js"></script>
+    <script src="./Bootswatch_Slate_files/bootswatch.js"></script>
+</body>
+
+</html>
+
