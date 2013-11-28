d6aa321eb974315d59d2624804c6bd43b4df848d
jbrelloch <jbrelloch@gmail.com>
2013-11-27 21:16:06 -0500

    New database scripts

--- create_Table_Script.sql
+++ create_Table_Script.sql
@@ -1,6 +1,6 @@
 CREATE TABLE members (
-  `m_id` varchar(50) NOT NULL,
-  `email` varchar(50) DEFAULT NULL,
+  `m_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL UNIQUE,
   `password` varchar(50) DEFAULT NULL,
   `first_name` varchar(50) DEFAULT NULL,
   `last_name` varchar(50) DEFAULT NULL,
@@ -12,8 +12,7 @@ CREATE TABLE members (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE addresses (
-  `a_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `m_id` varchar(50) NOT NULL,
+  `m_id` int(10) unsigned NOT NULL,
   `line_1` varchar(50) DEFAULT NULL,
   `line_2` varchar(50) DEFAULT NULL,
   `line_3` varchar(50) DEFAULT NULL,
@@ -22,7 +21,6 @@ CREATE TABLE addresses (
   `state` varchar(50) DEFAULT NULL,
   `country` varchar(50) DEFAULT NULL,
   `other` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`a_id`),
   FOREIGN KEY (`m_id`) REFERENCES members(m_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -38,11 +36,18 @@ CREATE TABLE books (
 
 CREATE INDEX title_index ON books(`title`) USING BTREE;
 
+CREATE TABLE orders (
+  `o_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `m_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`o_id`, `m_id`),
+  FOREIGN KEY (`m_id`) REFERENCES members(`m_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 CREATE TABLE transactions (
+  `o_id` int(10) unsigned NOT NULL,
   `b_id` int(10) unsigned NOT NULL,
   `quantity` int(10) unsigned NOT NULL,
-  `m_id` varchar(50) NOT NULL,
-  PRIMARY KEY (`b_id`, `m_id`),
-  FOREIGN KEY (`b_id`) REFERENCES books(`b_id`),
-  FOREIGN KEY (`m_id`) REFERENCES members(`m_id`)
+  PRIMARY KEY (`o_id`, `b_id`),
+  FOREIGN KEY (`o_id`) REFERENCES orders(`o_id`),
+  FOREIGN KEY (`b_id`) REFERENCES books(`b_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
